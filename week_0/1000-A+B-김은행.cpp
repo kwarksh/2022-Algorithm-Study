dfe2172e13838333a3f5b9f2cb95dedc3771bcea
Bank <87305109+kimbank@users.noreply.github.com>
2022-03-05 17:01:17 +0900

    0주차 제출

---
+++ week_0/1000-A+B-김은행.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short a, b;                 // 변수 선언
+    cin >> a >> b;              // a, b 입력
+    
+    cout << a + b << endl;      // a, b 합 출력
+    
+    return 0;
+}
